2ae45088b49cb55267199e8feac9e303399f5d69
Tristan van Vaalen <tristan.vanvaalen@student.uva.nl>
2016-04-15 16:56:26 +0200

    Silent building

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 
 # Author:   Tristan van Vaalen
 # Date:     April 2016
-# Comments: This is based on instructions found on:
-#           Instructions taken from: https://gist.github.com/gaoyifan/c881aa36cd02fb5c1c20
+# Comments: This script is built for Ubuntu 14.04.4:
+#            - http://releases.ubuntu.com/14.04/
+#           This is based on instructions found on:
+#           Instructions taken and modified from:
+#            - https://gist.github.com/gaoyifan/c881aa36cd02fb5c1c20
 #           !! Make sure you have at least 10GB free space in your root partition !!
 #           It is recommended to execute this on a VM! Use at your own risk.
 
@@ -13,34 +16,49 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# Display license info
+echo "RTAI on Unbuntu 14.04.4 Installer Copyright (C) 2016 Tristan van Vaalen"
+echo "This program comes with ABSOLUTELY NO WARRANTY; for details see license file."
+echo "This is free software, and you are welcome to redistribute it under certain conditions; see license file."
+read -rsp $'Press any key to start...\n' -n 1 key
+
 # We are installing this next to the other kernel sources
 cd /usr/src
 
 # Download and unpack RTAI 4.1 and kernel v3.10.32
-curl -L https://www.rtai.org/userfiles/downloads/RTAI/rtai-4.1.tar.bz2 | tar xj
-curl -L https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.10.32.tar.xz | tar xJ
-curl -L http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.32-saucy/linux-image-3.10.32-031032-generic_3.10.32-031032.201402221635_amd64.deb -o linux-image-3.10.32-generic-amd64.deb
+echo "Downloading RTAI 4.1..."
+curl -s -L https://www.rtai.org/userfiles/downloads/RTAI/rtai-4.1.tar.bz2 | tar xj
+echo "Downloading Linux 3.10.32..."
+curl -s -L https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.10.32.tar.xz | tar xJ
+echo "Downloading Linux image 3.10.32..."
+curl -s -L http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.10.32-saucy/linux-image-3.10.32-031032-generic_3.10.32-031032.201402221635_amd64.deb -o linux-image-3.10.32-generic-amd64.deb
 
 # Unpack the archive
-dpkg-deb -x linux-image-3.10.32-generic-amd64.deb linux-image-3.10.32-generic-amd64
+echo "Unpacking image..."
+dpkg-deb -x linux-image-3.10.32-generic-amd64.deb linux-image-3.10.32-generic-amd64 > /dev/null
 
 # Create symlinks to the files for ease of use (optional?)
 ln -s linux-3.10.32 linux
 ln -s rtai-4.1 rtai
 
 # Make sure we have all the packages
-apt-get update
-apt-get install --yes cvs subversion build-essential git-core g++-multilib gcc-multilib
-apt-get install --yes libtool automake libncurses5-dev kernel-package
+echo "Updating package list..."
+apt-get -qq update
+echo "Installing Linux build packages (if needed)..."
+apt-get -qq install --yes cvs subversion build-essential git-core g++-multilib gcc-multilib
+echo "Installing RTAI build packages (if needed)..."
+apt-get -qq install --yes libtool automake libncurses5-dev kernel-package
 
 # Copy the standard config
+echo "Copying standard config..."
 cp /usr/src/linux-image-3.10.32-generic-amd64/boot/config-3.10.32-031032-generic /usr/src/linux/.config
 
 # Move in the kernel source dir
 cd /usr/src/linux
 
 # Apply the RTAI kernel patch
-patch -p1 < /usr/src/rtai/base/arch/x86/patches/hal-linux-3.10.32-x86-5.patch
+echo "Patching kernel with RTAI..."
+patch -s -p1 < /usr/src/rtai/base/arch/x86/patches/hal-linux-3.10.32-x86-5.patch
 
 # Create the config file:
 echo "===============MENUCONFIG==============="
@@ -57,14 +75,17 @@ echo "Power Management and ACPI options"
 echo "    CPU idle PM support = DISABLE IT"
 echo "========================================"
 read -rsp $'Press any key to start menuconfig...\n' -n 1 key
-make menuconfig
+make -s menuconfig
 
 # Buidling kernel
+echo "Building kernel (takes a looong time)..."
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rtai
 
 # Installing kernel
-dpkg -i linux-image-3.10.32-rtai_3.10.32-rtai-1_amd64.deb
-dpkg -i linux-headers-3.10.32-rtai_3.10.32-rtai-1_amd64.deb
+echo "Installing linux image..."
+dpkg -i linux-image-3.10.32-rtai_3.10.32-rtai-1_amd64.deb > /dev/null
+echo "Installing linux headers..."
+dpkg -i linux-headers-3.10.32-rtai_3.10.32-rtai-1_amd64.deb > /dev/null
 
 # Patch grub to start our kernel next time
 GRUB_DEFAULT="saved"
@@ -74,7 +95,7 @@ GRUB_HIDDEN_TIMEOUT_QUIET="false"
 GRUB_CMDLINE_LINUX_DEFAULT="quiet splash lapic=notscdeadline panic=5"
 grub-set-default "Ubuntu, with Linux 4.2.0-27-generic"
 grub-reboot "Ubuntu, with Linux 3.10.32-rtai"
-update-grub
+update-grub > /dev/null
 
 echo "=========Installation complete!========="
 echo "The system will reboot to the RTAI kernel now!"
